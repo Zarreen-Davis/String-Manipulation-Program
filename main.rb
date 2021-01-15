79fdc4877999a3925e2948819d0b9e6367717872
Zarreen Davis <zarreen.davis@outlook.com>
2021-01-15 00:53:23 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,21 @@
+puts "Enter some text and I will change it from lowercase to uppercase"
+text = gets.chomp
+puts text.upcase
+puts "\n"
+
+puts "Enter some text and I will capitilise the first word"
+text = gets.chomp
+puts text.capitalize
+puts "\n"
+
+puts "Enter some text with upper and lowercases and I will swap the cases around. For example 'Are YOU happy TODAY'"
+text = gets.chomp
+puts text.swapcase
+puts "\n"
+
+
+puts "Enter some text and I will reverse it"
+text = gets.chomp
+puts text.reverse
+puts "\n"
+
